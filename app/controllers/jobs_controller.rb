55f719f6c112bf5c3d38a7f05ade7b40579353a8
climb719 <mary.oldweiler@gmail.com>
2021-09-09 10:12:09 -0400

    start create in job controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,4 +4,16 @@ class JobsController < ApplicationController
         jobs = Job.all
         render json: jobs
     end
+
+    def create 
+        job = Job.create(job_params)
+        render json: job
+    end
+
+    private
+
+    def job_params
+        byebug
+    end
 end
+
