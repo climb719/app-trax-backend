eb15244147b7f8cde65088150dbf324dbf42bd04
climb719 <mary.oldweiler@gmail.com>
2021-09-14 12:37:20 -0400

    user_id to job serializer

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,5 +1,5 @@
 class JobSerializer < ActiveModel::Serializer
-  attributes :id, :title, :company, :notes, :app_date, :link, :status_name, :status_id, :user_email
+  attributes :id, :title, :company, :notes, :app_date, :link, :status_name, :status_id, :user_email, :user_id
   
   def user_email
     self.object.user.email
