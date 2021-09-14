70e5efeef9fa5aee8a00868c23495413032fdda1
climb719 <mary.oldweiler@gmail.com>
2021-09-14 08:51:55 -0400

    user info to job serializer

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,3 +1,14 @@
 class JobSerializer < ActiveModel::Serializer
-  attributes :id, :title, :company, :notes, :app_date, :link, :status_name, :status_id
+  attributes :id, :title, :company, :notes, :app_date, :link, :status_name, :user_name, :user_email
+  
+
+  def user_name
+    self.object.user.name
+  end
+
+  def user_email
+    self.object.user.email
+  end
+
+
 end
